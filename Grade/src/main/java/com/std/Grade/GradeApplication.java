b90002809da5cec39242bdad4fa41290e9700b1a
sprab <sprab@Lenovo>
2026-05-18 09:08:25 +0530

    Initial commit

---
+++ Grade/src/main/java/com/std/Grade/GradeApplication.java
@@ -0,0 +1,13 @@
+package com.std.Grade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeApplication.class, args);
+	}
+
+}
